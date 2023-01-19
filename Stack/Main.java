ccfd476c4482a79ccea058dbc3a4da2991d4b790
hadi <hadi@depository.id>
2023-01-19 23:25:15 +0700

    add stack

--- Stack/Main.java
+++ Stack/Main.java
@@ -1,7 +1,24 @@
+
 package Stack;
+import java.util.*;
 class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.print("=======================\n\n");
+    Stack<String> stack = new Stack<String>();
+
+    // stack methods:
+    // 1. push => adding new item to the last index => return added item
+    // 2. pop => remove item from last index => return deleted item
+    stack.push("Reading Book");
+    stack.push("Swimming");
+    stack.push("Cooking");
+    stack.push("Watching Movie");
+    stack.pop();
+
+    // print stack
+    System.out.println(stack);
+
+    System.out.print("\n=======================\n");
   }
 }
---
+++ Stack/README.md
@@ -0,0 +1,10 @@
+## Defenition
+
+- Common known as LIFO(Last in First Out)
+
+## USE CASES
+
+- Undo/Redo feature on Text Editor
+- Moving Back/Forwad through Browser
+- Backtracking Algorithms(maze, file directories)
+- Calling Function (Call Ctack)
